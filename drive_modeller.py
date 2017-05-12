a1f63ac0a011ad959e5e832db74cf9f6a1bf78fb
Juha-Matti Tirilä <juha-matti.tirila@siili.com>
2017-05-12 11:52:37 +0300

    Read training set path from command line

--- drive_modeller.py
+++ drive_modeller.py
@@ -4,16 +4,21 @@ from scipy.misc import imresize
 import models.single_layer_dense as sld
 import models.alexnet as alex
 import os
+import sys
 
-SIMPLE_DATA_BASE_PATH = os.path.join(os.path.dirname(__file__), 'data', 'batch_1')
+SIMPLE_DATA_BASE_PATH = os.path.join(os.path.dirname(__file__),
+        os.pardir, 
+        os.pardir, 'data', 'batch_1')
 
 
 if __name__ == '__main__':
-    features, values = read_images_and_steering_angles(SIMPLE_DATA_BASE_PATH)
+    print("Argv 0: {}".format(sys.argv[0]))
+    assert len(sys.argv) == 2
+    features, values = read_images_and_steering_angles(sys.argv[1])
     features = features[:512]
     features = np.array(list(map(lambda x: imresize(x, (227, 227)), features)))
     values = values[:512]
     model = alex
     # model = sld
     # model = lenet
-    model.train_save(features, values)
+    # model.train_save(features, values)
