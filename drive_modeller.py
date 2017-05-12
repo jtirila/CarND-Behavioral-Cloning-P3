b8bb939433dc7a64d8340d48f4c0eac1b8bdebac
Juha-Matti Tirilä <juha-matti.tirila@siili.com>
2017-05-12 11:26:04 +0300

    WIP: work on getting alexnet to work, temporarily using only part of data.

--- drive_modeller.py
+++ drive_modeller.py
@@ -1,4 +1,6 @@
 from image_preprocessing.load_images import read_images_and_steering_angles
+import numpy as np
+from scipy.misc import imresize
 import models.single_layer_dense as sld
 import models.alexnet as alex
 import os
@@ -8,6 +10,10 @@ SIMPLE_DATA_BASE_PATH = os.path.join(os.path.dirname(__file__), 'data', 'batch_1
 
 if __name__ == '__main__':
     features, values = read_images_and_steering_angles(SIMPLE_DATA_BASE_PATH)
-    # sld.train_save(features, values)
-    alex.train_save(features, values)
-
+    features = features[:512]
+    features = np.array(list(map(lambda x: imresize(x, (227, 227)), features)))
+    values = values[:512]
+    model = alex
+    # model = sld
+    # model = lenet
+    model.train_save(features, values)
--- models/alexnet.py
+++ models/alexnet.py
@@ -1,18 +1,22 @@
 from keras.models import Model
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Input, merge, Reshape
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Input, merge, Lambda
+from keras import optimizers
+import tensorflow as tf
 import os
 
-CONCAT_AXIS = 1
+CONCAT_AXIS = 3
 
 
 def train_save(features, values):
-    inputs = Input(shape=(224, 224, 3))
-    resized_inputs = Reshape(input_shape=(160, 320), target_shape=(224, 224))(inputs)
-    branch_1_conv_1 = Conv2D(48, 11, 11, border_mode='same')(resized_inputs)
+    inputs = Input(shape=(227, 227, 3))
+    # resized_inputs = Lambda(lambda x: tf.image.resize_images(x, (227, 227)))(inputs)
+    # branch_1_conv_1 = Conv2D(48, 11, 11, border_mode='same')(resized_inputs)
+    branch_1_conv_1 = Conv2D(48, 11, 11, border_mode='same')(inputs)
     branch_1_maxpool_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same')(branch_1_conv_1)
     branch_1_conv_2 = Conv2D(128, 5, 5, border_mode='same')(branch_1_maxpool_1)
     branch_1_maxpool_2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same')(branch_1_conv_2)
 
+    # branch_2_conv_1 = Conv2D(48, 11, 11, border_mode='same')(resized_inputs)
     branch_2_conv_1 = Conv2D(48, 11, 11, border_mode='same')(inputs)
     branch_2_maxpool_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same')(branch_2_conv_1)
     branch_2_conv_2 = Conv2D(128, 5, 5, border_mode='same')(branch_2_maxpool_1)
@@ -31,27 +35,25 @@ def train_save(features, values):
     branch_2_maxpool_3 = MaxPooling2D(pool_size=(4, 4), strides=(4, 4), border_mode='same')(branch_2_conv_5)
 
     merge_2 = merge(inputs=[branch_1_maxpool_3, branch_2_maxpool_3], mode='concat', concat_axis=CONCAT_AXIS)
+    flat1 = Flatten()(merge_2)
 
-    branch_1_dense_1 = Dense(2048)(merge_2)
-    branch_2_dense_1 = Dense(2048)(merge_2)
+    branch_1_dense_1 = Dense(2048, activation='relu')(flat1)
+    branch_2_dense_1 = Dense(2048, activation='relu')(flat1)
 
-    merge_3 = merge(inputs=[branch_1_dense_1, branch_2_dense_1], mode='concat', concat_axis=CONCAT_AXIS)
+    merge_3 = merge(inputs=[branch_1_dense_1, branch_2_dense_1], mode='concat', concat_axis=0)
 
-    branch_2_dense_2 = Dense(1)(merge_3)
-    branch_1_dense_2 = Dense(1)(merge_3)
+    branch_2_dense_2 = Dense(2048, activation='relu')(merge_3)
+    branch_1_dense_2 = Dense(2048, activation='relu')(merge_3)
 
-    merge_4 = merge(inputs=[branch_1_dense_2, branch_2_dense_2], mode='concat', concat_axis=CONCAT_AXIS)
+    merge_4 = merge(inputs=[branch_1_dense_2, branch_2_dense_2], mode='concat', concat_axis=0)
 
-    output = Dense(1)(merge_4)
+    output = Dense(1, activation='relu')(merge_4)
 
     model = Model(input=inputs, output=output)
-    model.compile(loss='mse', optimizer='adam')
+    # sgd = optimizers.SDG(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='mean_squared_error', optimizer='sgd')
 
-    model.fit(features, values, validation_split=0.2, shuffle=True, nb_epoch=5)
+    model.fit(features, values, validation_split=0.0, shuffle=True, nb_epoch=5, batch_size=128)
     model.save(os.path.join(os.path.dirname(__file__), os.pardir, 'models', 'output', 'alexnet_model.h5'))
-
-
-
-
 
 
