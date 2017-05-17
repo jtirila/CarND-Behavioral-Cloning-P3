04bf34e3ebc7da0d79632fdda7103dc790b2b7da
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-05-17 15:12:32 +0300

    Trying a function rename to investigate a noMethodError issue

--- image_preprocessing/size_manipulations.py
+++ image_preprocessing/size_manipulations.py
@@ -1,13 +1,14 @@
-def resize_image(image, size):
+def my_resize_image(image, size):
     # https://github.com/fchollet/keras/issues/5298
     import tensorflow as tf
     return tf.image.resize_images(image, size)
 
+
 def resize_image_32_32(image):
-    return resize_image(image, (32, 32))
+    return my_resize_image(image, (32, 32))
 
 
 def resize_image_128_128(image):
-    return resize_image(image, (128, 128))
+    return my_resize_image(image, (128, 128))
 
 
