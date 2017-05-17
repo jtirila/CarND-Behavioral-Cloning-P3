f1b0eb3200928736c7cec20f9540bae04e34799c
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-05-17 15:16:22 +0300

    Working on solving method lookup error, FIX.

--- image_preprocessing/size_manipulations.py
+++ image_preprocessing/size_manipulations.py
@@ -7,6 +7,6 @@ def resize_image_32_32(image):
 def resize_image_128_128(image):
     # https://github.com/fchollet/keras/issues/5298
     import tensorflow as tf
-    return tf.image.resize_images(image, (32, 32))
+    return tf.image.resize_images(image, (128, 128))
 
 
