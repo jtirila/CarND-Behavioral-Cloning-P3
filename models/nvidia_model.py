de657aaee190339e1437d60cfd1c30c826fc1370
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-05-26 15:19:45 +0300

    Modify Nvidia a bit

--- models/nvidia_model.py
+++ models/nvidia_model.py
@@ -12,7 +12,7 @@ def train_save(features, values, nb_epoch=5):
 
     model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-
+    model.add(Lambda(enhance_contrast))
     model.add(Conv2D(24, 5, 5, subsample=(2,2), activation='relu'))
     model.add(Conv2D(36, 5, 5, subsample=(2,2), activation='relu'))
     model.add(Conv2D(48, 5, 5, subsample=(2,2), activation='relu'))
@@ -20,10 +20,11 @@ def train_save(features, values, nb_epoch=5):
     model.add(Conv2D(64, 3, 3, activation='relu'))
     model.add(Flatten())
     model.add(Dense(100))
+    model.add(Dropout(0.5))
     model.add(Dense(50))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.4))
     model.add(Dense(10))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     model.add(Dense(1))
 
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mse'])
