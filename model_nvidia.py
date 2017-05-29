0c87cac4ed2c5f8b91f70d055cfbb4479d333611
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-05-29 14:15:10 +0300

    Revert the model to the successful one.

--- model_nvidia.py
+++ model_nvidia.py
@@ -54,7 +54,7 @@ def train_save(features, values, nb_epoch=5):
     # Defining the model as in the lecture video, basically. Minor modifications to the cropping, dropout and
     # deep layer sizes
     model = Sequential()
-    model.add(Cropping2D(cropping=((67, 20), (5, 5)), input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 20), (8, 8)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
     model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Conv2D(36, 5, 5, subsample=(2, 2), activation='relu'))
@@ -62,11 +62,11 @@ def train_save(features, values, nb_epoch=5):
     model.add(Conv2D(64, 3, 3, activation='relu'))
     model.add(Conv2D(64, 3, 3, activation='relu'))
     model.add(Flatten())
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.35))
     model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.35))
     model.add(Dense(30, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.35))
     model.add(Dense(10, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
