f0a7d823cedeecd0f19fb53bfc8f41680fb2809a
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-05-29 21:12:32 +0300

    Modify normalization & dense layer width

--- model_nvidia.py
+++ model_nvidia.py
@@ -50,7 +50,7 @@ def train_save(features, values, nb_epoch=5):
     # deep layer sizes
     model = Sequential()
     model.add(Cropping2D(cropping=((70, 20), (8, 8)), input_shape=(160, 320, 3)))
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    model.add(Lambda(lambda x: (x / 128.0) - 1.0))
     model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Conv2D(36, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Conv2D(48, 5, 5, subsample=(2, 2), activation='relu'))
@@ -58,7 +58,7 @@ def train_save(features, values, nb_epoch=5):
     model.add(Conv2D(64, 3, 3, activation='relu'))
     model.add(Flatten())
     model.add(Dense(100))
-    model.add(Dense(30))
+    model.add(Dense(50))
     model.add(Dropout(0.35))
     model.add(Dense(10))
     model.add(Dropout(0.5))
